7553551dea638ce6e257eacf412686938f1e0114
Shreya <123@gmail.com>
2020-10-24 14:06:35 -0500

    Plinko

--- sketch.js
+++ sketch.js
@@ -1,9 +1,66 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+var particles = [];
+var plinkos = [];
+var divisions = [];
+var divisionHeight = 300;
+var ground;
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(480,800);
+  
+	engine = Engine.create();
+	world = engine.world;
+
+  ground = new Ground(240, 790, 480, 20);
+  
+  for(var k = 0; k <= width; k = k+80) {
+    divisions.push(new Division(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+
+  for(var j = 40; j<=width; j=j+50) {
+    plinkos.push(new Plinko(j,75,11));
+  }
+
+  for(var j = 15; j <=width-10; j=j+50) {
+    plinkos.push(new Plinko(j,175,11));
+  }
+
+  for(var j = 40; j <=width-10; j=j+50) {
+    plinkos.push(new Plinko(j,275,11));
+  }
+
+  for(var j = 15; j <=width-10; j=j+50) {
+    plinkos.push(new Plinko(j,375,11));
+  }
+
+  Engine.run(engine);
 }
 
 function draw() {
-  background(255,255,255);  
+  background("black");  
+
+  ground.display();
+
+  for(var j = 0; j < divisions.length; j++) {
+    divisions[j].display();
+  }
+
+  if(frameCount%60 === 0) {
+    particles.push(new Particle(random(width/2-10, width/2+10), 10, 10));
+  }
+
+  for(var j = 0; j < particles.length; j++) {
+    particles[j].display();
+ }
+
+  for(var j = 0; j < plinkos.length; j++) {
+    plinkos[j].display();
+  }
+
   drawSprites();
 }
